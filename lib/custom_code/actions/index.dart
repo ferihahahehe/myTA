598679e3731867e2e9f3d4bf5b05bfd5176bd097
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-06-21 09:17:32 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/index.dart
+++ lib/custom_code/actions/index.dart
@@ -4,7 +4,6 @@ export 'find_devices.dart' show findDevices;
 export 'get_connected_devices.dart' show getConnectedDevices;
 export 'get_rssi.dart' show getRssi;
 export 'is_bluetooth_enabled.dart' show isBluetoothEnabled;
-export 'receive_data.dart' show receiveData;
 export 'send_data.dart' show sendData;
 export 'request_bluetooth_on.dart' show requestBluetoothOn;
 export 'manage_data_listener.dart' show manageDataListener;
